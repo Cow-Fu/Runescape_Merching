cf7e0dd55645e68258d614d900e4a6874ae4b455
Nathan Justinian <nathanj758@gmail.com>
2018-03-19 16:26:26 -0400

    Added function for easy requests

--- src/grand_exchange_api/mod.rs
+++ src/grand_exchange_api/mod.rs
@@ -2,3 +2,19 @@
 
 pub mod catalogue;
 pub mod configuration;
+
+extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
+
+pub fn request<T>(url: &str) -> Option<T> where T: serde::de::DeserializeOwned {
+    let responce = reqwest::get(url);
+    match responce {
+        Ok(mut result) => {
+            let v: T = serde_json::from_str(result.text().unwrap().as_str()).unwrap();
+            return Some(v);
+        },
+        Err(_) => { },
+    }
+    return None;
+}
