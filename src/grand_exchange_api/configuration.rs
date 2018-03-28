2b5cae1663c3fea1631474b7e0b5c33a9fc33a52
Nathan Justinian <nathanj758@gmail.com>
2018-03-27 23:22:03 -0400

    Refactored

--- src/grand_exchange_api/configuration.rs
+++ src/grand_exchange_api/configuration.rs
@@ -3,6 +3,7 @@ extern crate reqwest;
 extern crate serde;
 extern crate serde_json;
 
+use grand_exchange_api::request;
 
 static BASE_URL: &str = "http://secure.runescape.com/m=itemdb_rs/api/info.json";
 
@@ -12,14 +13,5 @@ pub struct ConfigInfo {
 }
 
 pub fn get_last_update() -> Option<ConfigInfo> {
-    let responce = reqwest::get(BASE_URL);
-    match responce {
-        Ok(mut result) => {
-            let v: ConfigInfo = serde_json::from_str(result.text().unwrap().as_str()).unwrap();
-            // println!("{:?}", v.lastConfigUpdateRuneday);
-            return Some(v);
-        },
-        Err(_) => { println!("Hi"); },
-    }
-    return None;
+    return request::<ConfigInfo>(BASE_URL);
 }
